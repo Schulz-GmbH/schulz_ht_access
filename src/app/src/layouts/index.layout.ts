5cd8273c4c96258cc0f8f3d1f2033ea57f024714
Simon Marcel Linden <simon@MacBook-Air-von-Simon.local>
2024-12-02 13:18:21 +0100

    Rebase

--- src/app/src/layouts/index.layout.ts
+++ src/app/src/layouts/index.layout.ts
@@ -3,8 +3,6 @@ import { defineComponent } from "vue";
 // import { useRouter } from "vue-router";
 
 // Componetns
-import HeaderComponent from "../components/header/header.component.vue";
-import NavbarComponent from "../components/navbar/navbar.component.vue";
 
 /**
  * VueLayout - A Vue component that serves as the layout for the application.
@@ -18,62 +16,15 @@ import NavbarComponent from "../components/navbar/navbar.component.vue";
  */
 export default defineComponent({
 	name: "VueLayout",
-	components: {
-		HeaderComponent: HeaderComponent,
-		NavbarComponent: NavbarComponent,
-	},
+	components: {},
 	data() {
-		return {
-			isMenuOpen: false,
-			isNotifyOpen: false,
-			menuItems: [
-				{
-					title: "IoT Dashboard",
-					icon: "fas fa-home",
-					location: "/dashboard",
-				},
-				{
-					title: "Einstellungen",
-					icon: "fas fa-tools",
-					location: "/settings",
-				},
-				{ title: "Konsole", icon: "fas fa-terminal" },
-				{ title: "Geräte", icon: "fas fa-wifi" },
-				{ title: "Log-Dateien", icon: "fas fa-file" },
-				{ title: "DoistDes update", icon: "fas fa-upload" },
-				{ title: "Info", icon: "fas fa-info-circle" },
-			],
-			isMobile: window.innerWidth < 768,
-		};
+		return {};
 	},
 
-	setup() {
-		const isDevelopment = process.env.NODE_ENV === "development";
-		// const router = useRouter();
-
-		// Hier kannst du alle weiteren Lifecycle-Hooks oder Methoden hinzufügen.
-
-		return {
-			isDevelopment,
-		};
-	},
+	setup() {},
 	mounted() {
-		// Die `mounted`-Hook kann hier verwendet werden, um Logik auszuführen, wenn die Komponente gemountet ist.
 		console.log("VueLayout component mounted");
-		window.addEventListener("resize", this.handleResize);
 	},
-	beforeUnmount() {
-		window.removeEventListener("resize", this.handleResize);
-	},
-	methods: {
-		handleToggleMenu() {
-			this.isMenuOpen = !this.isMenuOpen;
-		},
-		handleToggleNotify() {
-			this.isNotifyOpen = !this.isNotifyOpen;
-		},
-		handleResize() {
-			this.isMobile = window.innerWidth < 768;
-		},
-	},
+	beforeUnmount() {},
+	methods: {},
 });
--- src/app/src/layouts/index.layout.vue
+++ src/app/src/layouts/index.layout.vue
@@ -1,21 +1,5 @@
 <template>
-	<!--  HEADER  -->
-	<header-component title="HS - Wirelesss Access" :is-menu-open="isMenuOpen" @toggle-menu="handleToggleMenu"
-		:is-notify-open="isNotifyOpen" @toggle-notify="handleToggleNotify" />
-
-	<section class="main-section h-full">
-		<navbar-component :is-mobile="isMobile" :is-menu-open="isMenuOpen" :menu-items="menuItems" />
-
-		<main class="main flex flex-col grow-[6] shrink-[1] basis-0 justify-between bg-background">
-			<div class="main-panel p-3">
-				<router-view />
-			</div>
-			<!--    FOOTER    -->
-			<footer class="footer">
-				<p class="footer__text">© 2024 Schulz Hygiene GmbH</p>
-			</footer>
-		</main>
-	</section>
+	<router-view />
 </template>
 
 <script src="./index.layout.ts"></script>
