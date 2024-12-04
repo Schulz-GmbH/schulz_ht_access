1edd43fee48d80b5df061f662cdf0f53e9369135
Simon Marcel Linden <linden@schulz-hygiene.de>
2024-12-04 12:21:26 +0100

    Vite Config: Cross-Origin Attribut entfernt mit neuem Plugin
    - Ein neues Plugin `removeCrossOriginPlugin` wurde in die `vite.config.ts` aufgenommen, um das `crossorigin` Attribut aus dem HTML zu entfernen.
    - Die Pluginliste der Vite-Konfiguration wurde aktualisiert, um `removeCrossOriginPlugin` nach dem `vue()` Plugin zu registrieren.
    - Diese Änderung stellt sicher, dass das `crossorigin` Attribut nicht automatisch zu Ressourcen hinzugefügt wird, was in bestimmten Szenarien zu Problemen führen könnte.
    
    Durch diese Anpassungen wird die Kontrolle über die HTML-Inhalte verbessert und mögliche Cross-Origin-Fehler beim Laden der Anwendung vermieden.

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -2,8 +2,20 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // import { resolve } from "path";
 import { fileURLToPath, URL } from "node:url";
+import { Plugin } from "vite";
+
+// Plugin zum Entfernen des `crossorigin` Attributs
+const removeCrossOriginPlugin = (): Plugin => {
+	return {
+		name: "remove-crossorigin",
+		transformIndexHtml(html) {
+			return html.replace(/\scrossorigin/g, "");
+		},
+	};
+};
+
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), removeCrossOriginPlugin()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
