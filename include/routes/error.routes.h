90192b7a098c92a0351cc4d6a66eabfa4a3b7bc5
Simon Marcel Linden <linden@schulz-hygiene.de>
2025-02-26 12:51:02 +0100

    Entfernen nicht verwendeten Codes und nicht benötigter Routen-Dateien
    Änderungen:
    
    - **Entfernung nicht verwendeter Header-Dateien**:
      - `basic.routes.h`, `error.routes.h`, `routes.h`, `wifi.routes.h` wurden entfernt. Diese Dateien enthalten nicht mehr benötigte Routen-Definitionen und Funktionen.
    
    - **Entfernung von Routen-Implementierungen**:
      - Alle Implementierungen der Routen, die in den entfernten Header-Dateien definiert waren, wurden ebenfalls gelöscht. Das betrifft insbesondere die Routen für das Laden von Dateien von der SD-Karte (`basic.routes.cpp`), Fehlerbehandlungsrouten (`error.routes.cpp`) und WLAN-bezogene Routen (`wifi.routes.cpp`).
    
    - **Vermeidung von unnötigem Code**:
      - Der entfernte Code betraf nicht mehr benötigte HTTP-Handler und das Einbinden von unnötigen Headern, die in der aktuellen Version der Anwendung nicht mehr verwendet werden. Dies verringert den Codeumfang und verbessert die Wartbarkeit.
    
    - **Wartungsfreundlichkeit**:
      - Durch das Entfernen dieser nicht verwendeten Routendefinitionen und Implementierungen wird die Wartung des Codes vereinfacht und die Übersichtlichkeit erhöht.
    
    Vorteile:
    - **Codebereinigung**: Der nicht verwendete Code wurde entfernt, was die Lesbarkeit und Wartbarkeit verbessert.
    - **Reduzierung der Dateigröße**: Durch das Entfernen unnötiger Routen und Header-Dateien wird die Größe des Projekts reduziert.
    - **Bessere Performance**: Weniger ungenutzter Code bedeutet, dass das Projekt in der Zukunft leichter gewartet und optimiert werden kann.
    
    ---

--- include/routes/error.routes.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERROR_ROUTES_H
-#define ERROR_ROUTES_H
-
-#include <ESPAsyncWebServer.h>
-
-void initErrorRoutes(AsyncWebServer &server);
-
-#endif
--- include/routes/routes.h
+++
@@ -1,8 +0,0 @@
-#ifndef ROUTES_H
-#define ROUTES_H
-
-#include "routes/basic.routes.h"
-#include "routes/error.routes.h"
-#include "routes/wifi.routes.h"
-
-#endif
